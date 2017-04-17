c7027f88b01bab57f2a405a12f68497e4e023399
bianhaiming <bianhaiming@gmail.com>
2017-04-17 22:54:45 +0800

    step 7 upload resume

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,7 +8,7 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
 
     if @job.is_hidden
-      flash[:warning] = "This Job already archieved"
+      flash[:warning] = "This Job already archived"
       redirect_to root_path
     end
   end

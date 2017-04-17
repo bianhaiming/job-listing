1e6fea70b14790254acaa52d7759f0cb1152de68
bianhaiming <bianhaiming@gmail.com>
2017-04-17 23:45:53 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   resources :jobs do
     resources :resumes
   end
@@ -12,7 +12,11 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+
+      resources :resumes
     end
   end
+
+
   root 'jobs#index'
 end
